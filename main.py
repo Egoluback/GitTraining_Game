c646bed6c61e5958e7a77a80b070cdf50a4670f5
Egoluback <46494602+Egoluback@users.noreply.github.com>
2020-01-28 19:59:48 +0300

    initial commit

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
-from World import World
-from Character import Character
-from Enemy import Enemy
-from Item import Item
+from Character.py import Character
+from Enemy.py import Enemy
+from Item.py import Item
 
-world = World()
-
-character = Character()
-
-while True:
-    new_world = world.Get()
-    user_pos = character.GetPos()
-    got_data = character.MakeStep(world.Get()[user_pos[0], user_pos[1]])
-    if (not got_data):
-        break
-    new_world[user_pos[0], user_pos[1]] = got_data
-    world.Update(new_world)
+for i in range(100):
+    
