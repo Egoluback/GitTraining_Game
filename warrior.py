9a4bad9c557c1130e2daabefc6d12779ab84b3a7
Alex011505 <alexm113131@gmail.com>
2020-01-29 20:04:57 +0300

    Added Warrior

---
+++ warrior.py
@@ -0,0 +1,26 @@
+class Warrior:
+    def __init__(self, name: str, hp: int, weapon: Weapon, shield: Shield):
+        self.name = name
+        self.hp = hp
+        self.weapon = weapon
+        self.shield = shield
+        self.IsAlive = True
+
+    def ChangeHp(self, x):
+        self.hp += x
+        if self.hp <= 0:
+            self.IsAlive = False
+        return self.IsAlive
+
+    def Attack(self, enemy: Warrior):
+        damage = self.weapon.GetDamage()
+        damage -= enemy.shield.GetDefense()
+        if damage < 0: damage = 0
+        has_killed = not enemy.ChangeHp(damage)
+        return has_killed
+
+    def GetHp(self):
+        return self.hp
+
+    def GetName(self):
+        return self.name
