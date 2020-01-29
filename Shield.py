274b91af6443ef2571db8bfea2b6d4c494513b13
diabeticonelove <58842899+diabeticonelove@users.noreply.github.com>
2020-01-29 20:03:20 +0300

    1 версия щита

---
+++ Shield.py
@@ -0,0 +1,18 @@
+import random
+
+class Shield():
+	def __init__(self, name, chance, defrange):
+		self.name = name
+		self.chance = chance
+		self.defrange = defrange
+	def getName(self):
+		return self.name
+	def GetDefense(self):
+		result = 0
+		number = random.randint(1, 101)
+		if(number < self.chance):
+			result = random.randint(self.defrange[0], self.defrange[1] + 1)
+		return result
+		
+		
+		
