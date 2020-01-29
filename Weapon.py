63546a7e460a64f515e34098e2e312577f068431
Egoluback <46494602+Egoluback@users.noreply.github.com>
2020-01-29 20:16:37 +0300

    fixed tab bugs

--- Weapon.py
+++ Weapon.py
@@ -1,13 +1,13 @@
 import random
-class Weapon():
-    def __init__(self,name,dices,edges):
-	    self.name = name
+class Weapon:
+	def __init__(self,name,dices,edges):
+		self.name = name
 		self.dices = dices
 		self.edges = edges
 	def getname(self):
 	    return self.name
 	def GetDamage(self):
-	    res = 0
-	    for i in range(0,self.dices):
-	        res += random.randint(1,self.edges+1)
+		res = 0
+		for i in range(0,self.dices):
+			res += random.randint(1,self.edges+1)
 		return res
